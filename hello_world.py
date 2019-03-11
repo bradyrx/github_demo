bae1c69cf10c01b0109424d47be3076eba319474
Riley X. Brady <riley.brady@colorado.edu>
2019-03-11 12:53:27 -0600

    import packages

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,14 @@
 """
 This is super complex, but prints out hello world.
 """
+import numpy as np
+import xarray as xr
+import climpred as cp
 def main():
     hello = 'hello'
     world = 'world'
     print(f'{hello} {world}: this is extra stuff.')
-
+    X = 3
 
 if __name__ == '__main__':
     main()
